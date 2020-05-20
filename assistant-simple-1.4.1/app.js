8ad30c2ded500b08462298fa5c484ef954eb0927
rd257 <65633152+rd257@users.noreply.github.com>
2020-05-19 19:56:31 -0700

    Add files via upload

---
+++ assistant-simple-1.4.1/app.js
@@ -0,0 +1,94 @@
+'use strict';
+
+var express = require('express'); // app server
+var bodyParser = require('body-parser'); // parser for post requests
+var AssistantV1 = require('watson-developer-cloud/assistant/v1'); // watson sdk
+var count = 0;
+var correct = 0;
+var incorrect = 0;
+var app = express();
+
+// Bootstrap application settings
+app.use(express.static('./public')); // load UI from public folder
+app.use(bodyParser.json());
+
+// Create the service wrapper
+
+var assistant = new AssistantV1({
+  version: '2018-07-10'
+});
+
+// Endpoint to be call from the client side
+app.post('/api/message', function (req, res) {
+  var workspace = process.env.WORKSPACE_ID || '<workspace-id>';
+  if (!workspace || workspace === '<workspace-id>') {
+    return res.json({
+      'output': {
+        'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
+      }
+    });
+  }
+  var payload = {
+    workspace_id: workspace,
+    context: req.body.context || {},
+    input: req.body.input || {}
+  };
+
+  // Send the input to the assistant service
+  assistant.message(payload, function (err, data) {
+    if (err) {
+      return res.status(err.code || 500).json(err);
+    }
+
+    return res.json(updateMessage(payload, data));
+  });
+});
+
+/**
+ * Updates the response text using the intent confidence
+ * @param  {Object} input The request to the Assistant service
+ * @param  {Object} response The response from the Assistant service
+ * @return {Object}          The response with the updated message
+ */
+function updateMessage(input, response) {
+  var responseText = null;
+  var str =  response.output.text[0];
+  if (!response.output) {
+    response.output = {};
+  } else {
+    //console.log(strict);
+    if(str.includes('1.')) {
+      //console.log("1st Question");
+      count++;
+      return response;
+    }
+    if(count == 5) {
+      response.output.text = "Your Score is: "; //+ response.output.text[0];
+      return response;
+    }
+    if(count >=1 && count < 5 ) {
+      count++;
+    }
+    console.log(count);
+    return response;
+  }
+  if (response.intents && response.intents[0]) {
+    var intent = response.intents[0];
+    // Depending on the confidence of the response the app can return different messages.
+    // The confidence will vary depending on how well the system is trained. The service will always try to assign
+    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
+    // user's intent . In these cases it is usually best to return a disambiguation message
+    // ('I did not understand your intent, please rephrase your question', etc..)
+    if (intent.confidence >= 0.75) {
+      responseText = 'I understood your intent was ' + intent.intent;
+    } else if (intent.confidence >= 0.5) {
+      responseText = 'I think your intent was ' + intent.intent;
+    } else {
+      responseText = 'I did not understand your intent';
+    }
+  }
+  response.output.text = responseText;
+  return response;
+}
+
+module.exports = app;
---
+++ assistant-simple-1.4.1/casper-runner.js
@@ -0,0 +1,40 @@
+require('dotenv').config({ silent: true });
+
+if (!process.env.WORKSPACE_ID) {
+  // eslint-disable-next-line
+  console.warn('Skipping casper tests because WORKSPACE_ID is null');
+  return;
+}
+
+var spawn = require('child_process').spawn;
+var app = require('./app');
+var port = 3000;
+
+var server = app.listen(port, function() {
+  // eslint-disable-next-line no-console
+  console.log('Server running on port: %d', port);
+
+  function kill(code) {
+    server.close(function() {
+      // eslint-disable-next-line no-process-exit
+      process.exit(code);
+    });
+  }
+
+  function runTests() {
+    var casper = spawn('npm', ['run', 'test-integration']);
+    casper.stdout.pipe(process.stdout);
+
+    casper.on('error', function(error) {
+      // eslint-disable-next-line
+      console.error(error);
+      server.close(function() {
+        process.exit(1);
+      });
+    });
+
+    casper.on('close', kill);
+  }
+
+  runTests();
+});
